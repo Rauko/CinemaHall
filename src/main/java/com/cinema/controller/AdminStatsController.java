446d5eca3a97ff6c3ed5263b24067b7afe202e24
Rauko <rauko.lord@gmail.com>
2026-02-13 21:23:53 +0200

    Controllers for export added

--- src/main/java/com/cinema/controller/AdminStatsController.java
+++ src/main/java/com/cinema/controller/AdminStatsController.java
@@ -1,9 +1,15 @@
 package com.cinema.controller;
 
+import com.cinema.model.User;
+import com.cinema.model.enums.ExportFormat;
 import com.cinema.model.enums.TicketStatus;
 import com.cinema.repository.MovieRepository;
+import com.cinema.repository.PurchaseHistoryRepository;
 import com.cinema.repository.TicketRepository;
 import com.cinema.repository.UserRepository;
+import com.cinema.service.export.PurchaseHistoryExportService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -16,13 +22,16 @@ public class AdminStatsController {
     private final UserRepository userRepository;
     private final MovieRepository movieRepository;
     private final TicketRepository ticketRepository;
+    private final PurchaseHistoryExportService purchaseHistoryExportService;
 
     public AdminStatsController(UserRepository userRepository,
                                 MovieRepository movieRepository,
-                                TicketRepository ticketRepository) {
+                                TicketRepository ticketRepository,
+                                PurchaseHistoryExportService purchaseHistoryExportService) {
         this.userRepository = userRepository;
         this.movieRepository = movieRepository;
         this.ticketRepository = ticketRepository;
+        this.purchaseHistoryExportService = purchaseHistoryExportService;
     }
 
     @GetMapping
@@ -33,5 +42,30 @@ public class AdminStatsController {
         stats.put("tickets", ticketRepository.count());
         stats.put("paidTickets", ticketRepository.countByStatus(TicketStatus.PAID));
         return stats;
+    }
+
+    @GetMapping("/users/{userId}/history/export")
+    public ResponseEntity<byte[]> exportUserHistory(
+            @PathVariable Long userId,
+            @RequestParam ExportFormat format
+    ){
+        User user = userRepository.findById(userId).get();
+
+        byte[] file = purchaseHistoryExportService.exportForAdmin(user, format);
+
+        String timestamp = java.time.LocalDateTime.now()
+                .withSecond(0)
+                .withNano(0)
+                .format(java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm"));
+
+        String filename = "user_" + userId + "_" + timestamp + "." +
+                format.name().toLowerCase();
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION,
+                        "attachment; filename=" + filename
+                )
+                .body(file);
+
     }
 }
--- src/main/java/com/cinema/controller/MeController.java
+++ src/main/java/com/cinema/controller/MeController.java
@@ -2,11 +2,16 @@ package com.cinema.controller;
 
 import com.cinema.dto.PurchaseHistoryDto;
 import com.cinema.dto.ticket.TicketDto;
+import com.cinema.model.enums.ExportFormat;
 import com.cinema.model.enums.TicketStatus;
 import com.cinema.model.User;
 import com.cinema.service.PurchaseHistoryService;
 import com.cinema.service.TicketService;
 import com.cinema.service.UserService;
+import com.cinema.service.export.PurchaseHistoryExportService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,13 +23,16 @@ public class MeController {
     private final TicketService ticketService;
     private final UserService userService;
     private final PurchaseHistoryService purchaseHistoryService;
+    private final PurchaseHistoryExportService purchaseHistoryExportService;
 
     public MeController(TicketService ticketService,
                         UserService userService,
-                        PurchaseHistoryService purchaseHistoryService) {
+                        PurchaseHistoryService purchaseHistoryService,
+                        PurchaseHistoryExportService purchaseHistoryExportService) {
         this.ticketService = ticketService;
         this.userService = userService;
         this.purchaseHistoryService = purchaseHistoryService;
+        this.purchaseHistoryExportService = purchaseHistoryExportService;
     }
 
     @GetMapping("/tickets")
@@ -61,4 +69,30 @@ public class MeController {
 
     }
 
+    @GetMapping("/history/export")
+    public ResponseEntity<byte[]> exportMyHistory(
+                            @RequestParam ExportFormat format,
+                            Authentication auth
+            ) {
+        byte[] file = purchaseHistoryExportService.exportForCurrentUser(format);
+
+        String username = auth.getName();
+
+        String timestamp = java.time.LocalDateTime.now()
+                .withSecond(0)
+                .withNano(0)
+                .format(java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm"));
+
+        String filename = String.format(
+                "history_%s-%s.%s",
+                username,
+                timestamp,
+                format.name().toLowerCase()
+        );
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION,
+                        "attachment; filename=" + filename)
+                .body(file);
+    }
 }
