0a3a2322f0ede7cfbda000829017fb49cbce59f2
Rauko <rauko.lord@gmail.com>
2026-03-01 22:36:51 +0200

    BaseAppException changed

--- src/main/java/com/cinema/exception/BaseAppException.java
+++ src/main/java/com/cinema/exception/BaseAppException.java
@@ -1,8 +1,17 @@
 package com.cinema.exception;
 
+import org.springframework.http.HttpStatus;
+
 public abstract class BaseAppException extends RuntimeException {
 
-    public BaseAppException(String message) {
+    private final HttpStatus status;
+
+    public BaseAppException(String message, HttpStatus status) {
         super(message);
+        this.status = status;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
     }
 }
