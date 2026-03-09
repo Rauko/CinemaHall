a0e6ac051b0025cd5a410e0f3a24e355f91a0c69
Rauko <rauko.lord@gmail.com>
2026-03-09 20:50:54 +0200

    ErrorResponse updated

--- src/main/java/com/cinema/exception/ErrorResponse.java
+++ src/main/java/com/cinema/exception/ErrorResponse.java
@@ -1,8 +1,11 @@
 package com.cinema.exception;
 
+import com.cinema.model.enums.ErrorCode;
+
 import java.time.LocalDateTime;
 
 public record ErrorResponse(
+        ErrorCode errorCode,
         String error,
         String message,
         LocalDateTime timestamp
