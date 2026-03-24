777e8fd3e8b68439a97220434c98413938ba4bbc
Rauko <rauko.lord@gmail.com>
2026-03-24 23:12:42 +0200

    AdminScreeningController refactored

--- src/main/java/com/cinema/controller/AdminScreeningController.java
+++ src/main/java/com/cinema/controller/AdminScreeningController.java
@@ -1,13 +1,13 @@
 package com.cinema.controller;
 
+import com.cinema.dto.screening.ScreeningDto;
+import com.cinema.dto.screening.mapper.ScreeningMapper;
+import com.cinema.dto.screening.request.CreateScreeningRequest;
 import com.cinema.model.Screening;
 import com.cinema.service.ScreeningService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDateTime;
-import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,20 +16,21 @@ public class AdminScreeningController {
 
     private final ScreeningService screeningService;
 
-    @PostMapping
-    public ResponseEntity<Screening> createScreening(@RequestBody Map<String,Object> request){
-        Long movieId = Long.valueOf(request.get("id").toString());
-        String hallName = request.get("hallName").toString();
-        LocalDateTime startTime = LocalDateTime.parse(request.get("startTime").toString());
-        int duration = Integer.parseInt(request.get("duration").toString());
-        double basePrice = Double.parseDouble(request.get("basePrice").toString());
+    @PostMapping("/create")
+    public ResponseEntity<ScreeningDto> createScreening(
+            @RequestBody CreateScreeningRequest request){
 
-        Screening screening = screeningService.addScreening(movieId, hallName, startTime, duration, basePrice);
-        return ResponseEntity.ok(screening);
+        Screening screening = screeningService.addScreening(
+                request.getMovieId(),
+                request.getHallName(),
+                request.getStartTime(),
+                request.getDuration(),
+                request.getPrice());
+        return ResponseEntity.ok(ScreeningMapper.toDto(screening));
     }
 
-    @DeleteMapping
-    public ResponseEntity<Void> deleteScreening(@RequestBody Long id){
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteScreening(@PathVariable Long id){
         screeningService.deleteScreening(id);
         return ResponseEntity.noContent().build();
     }
