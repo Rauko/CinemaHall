2f49d2c6663e8dad0cc7aec9d262af8588cd5fa1
Rauko <rauko.lord@gmail.com>
2026-04-22 23:08:03 +0300

    HallService updated, logging added

--- src/main/java/com/cinema/service/HallService.java
+++ src/main/java/com/cinema/service/HallService.java
@@ -6,6 +6,8 @@ import com.cinema.model.enums.SeatType;
 import com.cinema.repository.HallRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 @RequiredArgsConstructor
@@ -13,9 +15,16 @@ public class HallService {
 
     private final HallRepository hallRepository;
 
+    private static final Logger log = LoggerFactory.getLogger(HallService.class);
+
     public Hall createHallWithSeats(String name, int rows, int seatsPerRow) {
+
+        log.info("Creating hall: name={}, rows={}, seatsPerRow={}", name, rows, seatsPerRow);
+
         Hall hall = new Hall();
         hall.setName(name);
+
+        int totalSeats = 0;
 
         for (int r = 1; r <= rows; r++){
             for ( int s = 1; s <= seatsPerRow; s++){
@@ -27,8 +36,18 @@ public class HallService {
                 seat.setType(r == rows ? SeatType.VIP : SeatType.STANDARD);
 
                 hall.getSeats().add(seat);
+                totalSeats++;
             }
         }
-        return hallRepository.save(hall);
+
+        hallRepository.save(hall);
+
+        log.info("Hall created: id={}, name={}, totalSeats={}",
+                hall.getId(),
+                hall.getName(),
+                totalSeats
+        );
+
+        return hall;
     }
 }
