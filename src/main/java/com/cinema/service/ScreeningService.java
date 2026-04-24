a4a9573c5b6dc5c40b2c0a9fe0632cfa6935e211
Rauko <rauko.lord@gmail.com>
2026-04-24 23:44:39 +0300

    addScreening updated, logging added

--- src/main/java/com/cinema/service/ScreeningService.java
+++ src/main/java/com/cinema/service/ScreeningService.java
@@ -5,6 +5,8 @@ import com.cinema.model.Screening;
 import com.cinema.repository.ScreeningRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -14,6 +16,7 @@ import java.util.List;
 public class ScreeningService {
     private final ScreeningRepository screeningRepository;
     private final MovieService movieService;
+    private static final Logger log = LoggerFactory.getLogger(ScreeningService.class);
 
     public List<Screening> findAllScreenings() {
         return screeningRepository.findAll();
@@ -29,7 +32,18 @@ public class ScreeningService {
                                   LocalDateTime startTime,
                                   int duration,
                                   double price) {
-        Movie movie = movieService.getMovieById(movieId);
+
+        log.info("Creating screening: movieId={}, hallName={}, startTime={}, duration={}, price={}",
+                movieId, hallName, startTime, duration, price);
+
+        Movie movie;
+
+        try {
+            movie = movieService.getMovieById(movieId);
+        } catch (Exception e) {
+            log.warn("Movie not found for screening: movieId={}", movieId);
+            throw e;
+        }
 
         Screening screening = new Screening();
         screening.setMovie(movie);
@@ -37,6 +51,12 @@ public class ScreeningService {
         screening.setStartTime(startTime);
         screening.setDuration(duration);
         screening.setBasePrice(price);
+
+        log.info("Screening created: id={}, movieId={}, hallName={}",
+                screening.getId(),
+                movieId,
+                hallName
+        );
 
         return screeningRepository.save(screening);
     }
