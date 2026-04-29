4308dfde5dcdb6db840a4f9174976bc214473746
Rauko <rauko.lord@gmail.com>
2026-04-29 23:27:45 +0300

    MovieService  updated

--- src/main/java/com/cinema/service/MovieService.java
+++ src/main/java/com/cinema/service/MovieService.java
@@ -6,26 +6,48 @@ import com.cinema.exception.MovieNotFoundException;
 import com.cinema.model.Movie;
 import com.cinema.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-
 @Service
 @RequiredArgsConstructor
 public class MovieService {
+
+    private static final Logger log = LoggerFactory.getLogger(MovieService.class);
+
     private final MovieRepository movieRepository;
 
     public List<Movie> getAllMovies() {
-        return movieRepository.findAll();
+        log.info("Fetching all movies");
+
+        List<Movie> movies = movieRepository.findAll();
+
+        log.debug("Movies fetched: count={}", movies.size());
+
+        return movies;
     }
 
-    public Movie getMovieById(Long id){
+    public Movie getMovieById(Long id) {
+
+        log.debug("Fetching movie by id={}", id);
+
         return movieRepository.findById(id)
-                .orElseThrow(() -> new MovieNotFoundException(id));
+                .orElseThrow(() -> {
+                    log.warn("Movie not found: id={}", id);
+                    return new MovieNotFoundException(id);
+                });
     }
 
     public Movie createMovie(CreateMovieRequest request) {
+
+        log.info("Creating movie: title={}, releaseYear={}",
+                request.getTitle(),
+                request.getReleaseYear()
+        );
+
         Movie movie = new Movie();
         movie.setTitle(request.getTitle());
         movie.setDescription(request.getDescription());
@@ -35,25 +57,58 @@ public class MovieService {
         movie.setDirector(request.getDirector());
         movie.setPosterUrl(request.getPosterUrl());
 
-        return movieRepository.save(movie);
+        movieRepository.save(movie);
+
+        log.info("Movie created: id={}, title={}",
+                movie.getId(),
+                movie.getTitle()
+        );
+
+        return movie;
     }
 
     public Movie updateMovie(Long id, UpdateMovieRequest request) {
 
+        log.info("Updating movie: id={}", id);
+
         Movie movie = getMovieById(id);
 
-        if (request.getTitle() != null)  movie.setTitle(request.getTitle());
-        if (request.getDescription() != null)  movie.setDescription(request.getDescription());
-        if (request.getGenres() != null)  movie.setGenres(request.getGenres());
-        if (request.getDuration() != null)  movie.setDuration(request.getDuration());
-        if (request.getReleaseYear() != null)  movie.setReleaseYear(request.getReleaseYear());
-        if (request.getDirector() != null)  movie.setDirector(request.getDirector());
-        if (request.getPosterUrl() != null)  movie.setPosterUrl(request.getPosterUrl());
+        if (request.getTitle() != null) movie.setTitle(request.getTitle());
+        if (request.getDescription() != null) movie.setDescription(request.getDescription());
+        if (request.getGenres() != null) movie.setGenres(request.getGenres());
+        if (request.getDuration() != null) movie.setDuration(request.getDuration());
+        if (request.getReleaseYear() != null) movie.setReleaseYear(request.getReleaseYear());
+        if (request.getDirector() != null) movie.setDirector(request.getDirector());
+        if (request.getPosterUrl() != null) movie.setPosterUrl(request.getPosterUrl());
 
-        return movieRepository.save(movie);
+        movieRepository.save(movie);
+
+        log.info("Movie updated: id={}, title={}",
+                movie.getId(),
+                movie.getTitle()
+        );
+
+        return movie;
     }
 
-    public void deleteMovie(Long id){
-        movieRepository.deleteById(id);
+    public void deleteMovie(Long id) {
+
+        Movie movie = movieRepository.findById(id)
+                .orElseThrow(() -> {
+                    log.warn("Movie not found for deletion: id={}", id);
+                    return new MovieNotFoundException(id);
+                });
+
+        log.info("Deleting movie: id={}, title={}",
+                movie.getId(),
+                movie.getTitle()
+        );
+
+        movieRepository.delete(movie);
+
+        log.info("Movie deleted: id={}, title={}",
+                movie.getId(),
+                movie.getTitle()
+        );
     }
 }
